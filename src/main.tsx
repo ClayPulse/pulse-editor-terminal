e72372036f510d54dafac2795482de8ecd7e98d9
Shellishack <github-action@actions-template-sync.noreply.github.com>
2025-09-01 01:02:11 +0000

    chore(template): merge template changes :up:
    Signed-off-by: Shellishack <github-action@actions-template-sync.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./tailwind.css";
 import config from "../pulse.config";
-import TerminalPanel from "./components/terminal-panel";
+import { useLoading } from "@pulse-editor/react-api";
 
 export const Config = config;
 
 export default function Main() {
+  const [count, setCount] = useState<number>(0);
+  const { isReady, toggleLoading } = useLoading();
+
+  useEffect(() => {
+    if (isReady) {
+      toggleLoading(false);
+    }
+  }, [isReady, toggleLoading]);
+
   return (
-    <div className="w-full h-full flex flex-col">
-      <TerminalPanel />
+    <div className="p-2 flex flex-col">
+      <div className="flex items-center gap-x-1">
+        GitHub:
+        <button
+          className="w-8 h-8 border-1 border-gray-300 rounded-full p-1 hover:bg-gray-100"
+          onClick={() => {
+            window.open(
+              "https://github.com/claypulse/pulse-app-template",
+              "_blank"
+            );
+          }}
+        >
+          <img
+            src="assets/github-mark-light.svg"
+            alt="GitHub"
+            className="w-full h-full"
+          />
+        </button>
+      </div>
+
+      <div>
+        <button
+          className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-sm"
+          onClick={() => setCount(count + 1)}
+        >
+          Click me
+        </button>
+      </div>
+      <p className="text-blue-400">{count}</p>
     </div>
   );
 }
